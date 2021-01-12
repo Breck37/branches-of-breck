b5882af7c938041c9748b9c13c14166f15d4308d
B-eck37 <brent.eckert7@gmail.com>
2021-01-11 20:39:36 -0700

    feat: adding project data for dynamic project page

---
+++ pages/api/projects.js
@@ -0,0 +1,24 @@
+// PROJECT TEMPLATE //
+// {
+// key: "1",
+//     name: "",
+//     tagline: "",
+//     technologies: "",
+//     description: "",
+//     image: "",
+//     website: "",
+//     githubRepo:
+// }
+
+export const projectsData = [
+    {
+        key: "breck",
+        name: "Breckulator",
+        tagline: "Simple calculator modeled after the Victor 700",
+        technologies: ["React", "Typescript", "Sass"],
+        description: "",
+        image: "/breckulator.png",
+        website: "https://breckulator.com",
+        githubRepo: "https://github.com/Breck37/breckulator"
+    }
+]
--- pages/projects.js
+++ pages/projects.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import styles from '../styles/Home.module.css'
+import { projectsData } from './api/projects';
 
 const Projects = () => {
     return (
         <div>
-            <div className={styles.links}>
-                <a rel="stylesheet" href="https://breckulator.com">
-                    <div className={styles.project}>
-                        Breckulator
-              <img src="/breckulator.png" alt="breckulator project" />
-                    </div>
-                </a>
-            </div>
+            {projectsData.map((project) => (
+                <div key={project.key} className={styles.links}>
+                    <a rel="stylesheet" href={project.website} target="_blank">
+                        <div className={styles.project}>
+                            {project.name}
+                            <img src={project.image} alt="breckulator project" />
+                        </div>
+                    </a>
+                </div>
+            ))}
         </div>
     )
 }
