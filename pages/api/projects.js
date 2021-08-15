64e9bf9eed9f8420b9ae22aece67a00a141cab83
Brent Eckert <brent.eckert7@gmail.com>
2021-08-15 00:06:37 -0700

    feat: include css demo project, include imageWidth / alt fields
    Signed-off-by: Brent Eckert <brent.eckert7@gmail.com>

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -15,10 +15,24 @@ export const projectsData = [
         key: "breck",
         name: "Breckulator",
         tagline: "Simple calculator modeled after the Victor 700",
-        technologies: ["React", "Typescript", "Sass"],
+        technologies: ["React", "Typescript", "Sass", "Vercel"],
         description: "",
         image: "/breckulator.png",
         website: "https://breckulator.com",
-        githubRepo: "https://github.com/Breck37/breckulator"
-    }
+        githubRepo: "https://github.com/Breck37/breckulator",
+        alt: "breckulator project image",
+        imageWidth: 150,
+    },
+    {
+        key: "css-for-js",
+        name: "CSS For JS Developers Demo",
+        tagline: "Simple demo of CSS from a JS developers perspective",
+        technologies: ["React", "Next", "Vercel"],
+        description: "",
+        image: "/css-for-js-dev.png",
+        website: "https://css-for-js-demo-sibi.vercel.app/",
+        githubRepo: "https://github.com/Breck37/css-for-js-demo",
+        alt: "css for js project image",
+        imageWidth: 200,
+    },
 ]
--- pages/projects.js
+++ pages/projects.js
@@ -1,22 +1,54 @@
-import React from 'react'
-import styles from '../styles/Home.module.css'
-import { projectsData } from './api/projects';
+import React from "react";
+import styles from "../styles/Home.module.css";
+import styled from "styled-components";
+import { projectsData } from "./api/projects";
 
 const Projects = () => {
-    return (
-        <div>
-            {projectsData.map((project) => (
-                <div key={project.key} className={styles.links}>
-                    <a rel="stylesheet" href={project.website} target="_blank">
-                        <div className={styles.project}>
-                            {project.name}
-                            <img src={project.image} alt="breckulator project" />
-                        </div>
-                    </a>
-                </div>
-            ))}
-        </div>
-    )
-}
+  return (
+    <ProjectWrap>
+      {projectsData.map((project) => (
+        <ProjectCard key={project.key} imageWidth={project.imageWidth}>
+          <a rel="stylesheet" href={project.website} target="_blank">
+            <div className={styles.project}>
+              {project.name}
+              <img src={project.image} alt={project.alt} />
+            </div>
+            <div>
+                {project.tagline}
+            </div>
+          </a>
+        </ProjectCard>
+      ))}
+    </ProjectWrap>
+  );
+};
 
 export default Projects;
+
+const ProjectWrap = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+`;
+
+const ProjectCard = styled.div`
+  margin-top: 24px;
+  text-align: center;
+
+  > a > div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    > img {
+        height: 200px;
+        width: ${({ imageWidth }) => imageWidth ?? 150}px;
+        margin-top: 16px;
+    }
+  }
+
+  > a div:last-child {
+      margin: auto;
+      margin-top: 16px;
+      max-width: 40%;
+  }
+`;
